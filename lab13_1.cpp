b3dec112f9db91fbe5be276084c44d1093f4e966
JWJY <warisara.jy@gmail.com>
2023-01-10 15:09:36 +0700

    1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int x){
+    int y;
+        if(x<=1) return x;
+        else{
+            return y = fibonacci(x-1) + fibonacci(x-2);
+        }
+
+}
+
+
